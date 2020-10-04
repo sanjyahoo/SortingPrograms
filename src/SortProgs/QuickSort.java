41d09bea40fbe27fed8b63501af298c5cc959594
sanjay yahoo <sjosjoysos@gmail.com>
2020-10-04 18:19:02 +0530

    some changes

--- src/SortProgs/QuickSort.java
+++ src/SortProgs/QuickSort.java
@@ -1,7 +1,27 @@
 package SortProgs;
 
 public class QuickSort {
-public static void main(String[] args) {
 	
-}
+	int quicklySortThis(int arr[], int low, int high){
+		int pivot = arr[(low-high)/2];
+		while(low<=high){
+		
+		while(arr[low]<pivot){
+			high--;
+		}
+		while(arr[high]>pivot){
+			high--;
+		}
+		if(low<=high){
+			int temp=arr[low];
+			arr[low]=arr[high];
+			arr[high]=temp;
+		}
+		}
+		return low;
+	}
+
+	public static void main(String[] args) {
+	
+	}
 }
